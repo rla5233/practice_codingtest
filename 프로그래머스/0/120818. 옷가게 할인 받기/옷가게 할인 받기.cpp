7b3dc07209fb90abd72b44877a07531a69d12dac
KimSooWon <rla5233@naver.com>
2024-10-06 04:02:08 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
@@ -4,18 +4,15 @@ int solution(int Price)
 {
 	if (500000 <= Price)
 	{
-		Price *= 80;
-		Price /= 100;
+		Price *= 0.8;
 	}
 	else if (300000 <= Price)
 	{
-		Price *= 90;
-		Price /= 100;
+		Price *= 0.9;
 	}
 	else if (100000 <= Price)
 	{
-		Price *= 95;
-		Price /= 100;
+		Price *= 0.95;
 	}
 
     return Price;
