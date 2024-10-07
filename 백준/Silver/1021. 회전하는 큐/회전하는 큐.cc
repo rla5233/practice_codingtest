7d31d162d14b739f995de43b7bb65045dee5d2d9
SooWon Kim <rla5233@naver.com>
2024-10-07 20:28:49 +0900

    [Silver III] Title: 회전하는 큐, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <deque>
+#include <numeric>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+
+    int N = 0, M = 0;
+    std::cin >> N >> M;
+
+    int Result = 0;
+    std::deque<int> dq(N, 0);
+    std::deque<int>::iterator it = dq.begin();
+    std::iota(dq.begin(), dq.end(), 1);
+    while (M--)
+    {
+        int Out = 0;
+        std::cin >> Out;
+
+        std::deque<int>::iterator Findit = std::find(dq.begin(), dq.end(), Out);
+        int Dist1 = std::abs(std::distance(it, Findit));
+        int Dist2 = std::distance(dq.begin(), it) + std::distance(Findit, dq.end());
+        int Dist3 = std::distance(dq.begin(), Findit) + std::distance(it, dq.end());
+        Result += std::min(std::min(Dist1, Dist2), Dist3);
+        it = dq.erase(Findit);
+    }
+
+    std::cout << Result;
+
+    return 0;
+}
