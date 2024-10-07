05cb0c7e066a534aa381ca139dc4b1eb3e7e8d34
SooWon Kim <rla5233@naver.com>
2024-10-08 01:13:53 +0900

    [level 0] Title: a와 b 출력하기, Time: 3.89 ms, Memory: 4.45 MB -BaekjoonHub

---
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(void) {
+    int a;
+    int b;
+    std::cin >> a >> b;
+    std::cout << "a = " << a;
+    std::cout << std::endl;
+    std::cout << "b = " << b;
+    return 0;
+}
