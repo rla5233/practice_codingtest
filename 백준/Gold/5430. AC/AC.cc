3cd5414e08f7e4795b64b770e313e6b5e8f665f7
SooWon Kim <rla5233@naver.com>
2024-10-07 22:22:58 +0900

    [Gold V] Title: AC, Time: 44 ms, Memory: 3080 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.cc
+++ 백준/Gold/5430. AC/AC.cc
@@ -1,87 +1,85 @@
 #include <iostream>
-#include <sstream>
 #include <deque>
-#include <algorithm>
 
 int main()
 {
     std::ios_base::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
     
     int T = 0; std::cin >> T;
-
     while (T--)
     {
         std::string P = ""; std::cin >> P;
         int N = 0; std::cin >> N;
         std::string NumList = ""; std::cin >> NumList;
-        std::replace_if(NumList.begin(), NumList.end(), [](const char& C) { return !isdigit(C); }, ' ');
         
         std::deque<int> Array;
         std::string Temp = "";
-        std::stringstream SS(NumList);
-        while (SS >> Temp)
+        for (char C : NumList)
         {
-            Array.push_back(std::stoi(Temp));
+            if (isdigit(C))
+            {
+                Temp.push_back(C);
+                continue;
+            }
+
+            if (!Temp.empty())
+            {
+                Array.push_back(std::stoi(Temp));
+                Temp = "";
+            }
         }
 
-        int RCount = 0;
-        bool bError = false;
+        bool bError = false, bReverse = false;
         for (char C : P)
         {
             if ('R' == C)
             {
-                ++RCount;
+                bReverse = !bReverse;
+                continue;
             }
-            else
-            {
-                if (Array.empty())
-                {
-                    bError = true;
-                    break;
-                }
 
-                (0 != RCount % 2) ? Array.pop_back() : Array.pop_front();
+            if (Array.empty())
+            {
+                bError = true;
+                break;
             }
+
+            bReverse ? Array.pop_back() : Array.pop_front();
         }
 
         if (bError)
         {
             std::cout << "error\n";
+            continue;
+        }
+         
+        std::cout << "[";
+        if (bReverse)
+        {
+            for (int i = Array.size() - 1; i >= 0; --i)
+            {
+                std::cout << Array[i];
+                if (0 != i)
+                {
+                    std::cout << ",";
+                }
+            }
         }
         else
         {
-            std::cout << "[";
-
-            if (RCount % 2)
+            for (int i = 0; i < Array.size(); ++i)
             {
-                for (int i = Array.size() - 1; i >= 0; --i)
+                if (Array.size() - 1 != i)
                 {
-                    if (0 != i)
-                    {
-                        std::cout << Array[i] << ",";
-                    }
-                    else
-                    {
-                        std::cout << Array[i];
-                    }
+                    std::cout << Array[i] << ",";
+                }
+                else
+                {
+                    std::cout << Array[i];
                 }
             }
-            else
-            {
-                for (int i = 0; i < Array.size(); ++i)
-                {
-                    if (Array.size() - 1 != i)
-                    {
-                        std::cout << Array[i] << ",";
-                    }
-                    else
-                    {
-                        std::cout << Array[i];
-                    }
-                }
-            }
-            std::cout << "]\n";
         }
+        std::cout << "]\n";
     }
 
     return 0;
