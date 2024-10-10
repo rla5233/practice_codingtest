9a5edbab8d16c93bfd3c3136b7d01327a1615608
SooWon Kim <rla5233@naver.com>
2024-10-10 21:01:17 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.cpp
+++ 프로그래머스/1/42862. 체육복/체육복.cpp
@@ -1,44 +1,23 @@
-#include <algorithm>
 #include <vector>
-#include <set>
 
 int solution(int N, std::vector<int> Lost, std::vector<int> Reserve)
 {
-    std::set<int> LostNum, ResNum;
-    for (int Num : Reserve)
-    {
-        ResNum.insert(Num);
-    }
+    std::vector<int> Student(32, 0);
+    for (int Num : Lost) --Student[Num];
+    for (int Num : Reserve) ++Student[Num];
 
-    int Result = N - Lost.size();
-    for (int Num : Lost)
+    int Result = 0;
+    for (int i = 1; i <= N; i++)
     {
-        if (ResNum.end() != ResNum.find(Num))
+        if (-1 == Student[i])
         {
-            ResNum.erase(ResNum.find(Num));
-            ++Result;
-        }
-        else
-        {
-            LostNum.insert(Num);
-        }
-    }
-    
-    for (int Num : LostNum)
-    {       
-        if (ResNum.end() != ResNum.find(Num - 1))
-        {
-            ResNum.erase(ResNum.find(Num - 1));
-            ++Result;
-            continue;
+            if (1 == Student[i - 1]) 
+                Student[i] = (Student[i - 1] = 0);
+            else if (1 == Student[i + 1]) 
+                Student[i] = (Student[i + 1] = 0);
         }
 
-        if (ResNum.end() != ResNum.find(Num + 1))
-        {
-            ResNum.erase(ResNum.find(Num + 1));
-            ++Result;
-            continue;
-        }
+        if (0 <= Student[i]) ++Result;
     }
 
     return Result;
