1930ec16e4f21f9f4acc2ed282c936e30769343f
SooWon Kim <rla5233@naver.com>
2024-10-11 13:43:10 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
@@ -1,25 +1,19 @@
 #include <string>
 #include <vector>
 
-void MakeSecretMap(std::vector<std::string>& Map, int MapIndex, int Num)
-{
-    int Idx = Map.size() - 1;
-    while (0 != Num)
-    {
-        if (1 == Num % 2)  Map[MapIndex][Idx] = '#';
-        Num >>= 1;
-        --Idx;
-    }
-}
-
 std::vector<std::string> solution(int N, std::vector<int> Arr1, std::vector<int> Arr2) 
 {
     std::vector<std::string> Result(N, std::string(N, ' '));
-
     for (int i = 0; i < N; ++i)
     {
-        MakeSecretMap(Result, i, Arr1[i]);
-        MakeSecretMap(Result, i, Arr2[i]);
+        int Num = Arr1[i] | Arr2[i];
+        int Idx = N - 1;
+        while (0 != Num)
+        {
+            if (1 == Num % 2) Result[i][Idx] = '#';
+            Num >>= 1;
+            --Idx;
+        }
     }
 
     return Result;
