887eefc01e6448bc8d0e637e7b0f73d8a6688832
SooWon Kim <rla5233@naver.com>
2024-10-12 01:50:16 +0900

    [level 1] Title: 직사각형 별찍기, Time: 2.64 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.cpp
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.cpp
@@ -4,13 +4,8 @@ int main(void)
 {
     int N = 0, M = 0;
     std::cin >> N >> M;
-    for (int y = 0; y < M; ++y)
-    {
-        for (int x = 0; x < N; ++x)
-        {
-            std::cout << "*";
-        }
-        
-        std::cout << "\n";
-    }
+
+    std::string Star(N, '*');
+    for (int x = 0; x < M; ++x)
+        std::cout << Star << "\n";
 }
