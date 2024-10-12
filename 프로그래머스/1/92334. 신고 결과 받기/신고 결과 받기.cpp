9d5493499beb902821b336abc7d99bbe158b569a
SooWon Kim <rla5233@naver.com>
2024-10-12 22:25:36 +0900

    [level 1] Title: 신고 결과 받기, Time: 182.86 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
@@ -1,35 +1,59 @@
-#include <unordered_map>
-#include <unordered_set>
-#include <sstream>
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <unordered_set>
 
-std::vector<int> solution(std::vector<std::string> ID_List, std::vector<std::string> Report, int K) 
-{
-    std::unordered_map<std::string, std::unordered_set<std::string>> ReportInfo;
-    std::unordered_map<std::string, int> ReportCount;
-    for (const std::string& Info : Report)
-    {
-        std::string ID = "", RepID = "";
-        std::stringstream SS(Info);
-        SS >> ID >> RepID;
-        ReportCount[RepID] += ReportInfo[ID].insert(RepID).second;
+using namespace std;
+
+vector<int> solution(vector<string> id_list, vector<string> report, int k) {
+    // 중복 신고를 제거하기 위해 각 유저별로 신고한 유저를 저장하는 맵
+    unordered_map<string, unordered_set<string>> user_reports;
+    
+    // 각 유저가 신고한 유저 집합을 초기화
+    for(const string& user : id_list){
+        user_reports[user] = unordered_set<string>();
     }
-
-    std::vector<int> Result;
-    for (const std::string& ID : ID_List)
-    {
-        int MailCount = 0;
-        if (ReportInfo.end() != ReportInfo.find(ID))
-        {
-            for (const std::string& Info : ReportInfo[ID])
-            {
-                MailCount += (K <= ReportCount[Info]);
+    
+    // 신고 정보 처리: 중복 신고 제거
+    for(const string& rep : report){
+        int space_pos = rep.find(' ');
+        string reporter = rep.substr(0, space_pos);
+        string reportee = rep.substr(space_pos + 1);
+        user_reports[reporter].insert(reportee);
+    }
+    
+    // 각 유저별 신고당한 횟수를 저장할 맵
+    unordered_map<string, int> report_count;
+    for(const string& user : id_list){
+        report_count[user] = 0;
+    }
+    
+    // 신고당한 횟수 집계
+    for(const auto& [reporter, reportees] : user_reports){
+        for(const string& reportee : reportees){
+            report_count[reportee]++;
+        }
+    }
+    
+    // 정지 대상 유저 집합
+    unordered_set<string> suspended_users;
+    for(const auto& [user, count] : report_count){
+        if(count >= k){
+            suspended_users.insert(user);
+        }
+    }
+    
+    // 각 유저별로 받은 메일 수 계산
+    vector<int> answer;
+    for(const string& user : id_list){
+        int mails = 0;
+        for(const string& reportee : user_reports[user]){
+            if(suspended_users.find(reportee) != suspended_users.end()){
+                mails++;
             }
         }
-
-        Result.push_back(MailCount);
+        answer.push_back(mails);
     }
-
-    return Result;
+    
+    return answer;
 }
