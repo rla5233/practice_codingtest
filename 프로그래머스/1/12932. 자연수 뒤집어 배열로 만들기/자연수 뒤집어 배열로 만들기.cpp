031c6f2723dbf672fcfd1bd6410b0f00dee495e1
SooWon Kim <rla5233@naver.com>
2024-10-14 10:07:11 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(long long n) 
+{
+    vector<int> answer;
+    while (0 != n)
+    {
+        answer.push_back(n % 10);
+        n /= 10;
+    }
+
+    return answer;
+}
