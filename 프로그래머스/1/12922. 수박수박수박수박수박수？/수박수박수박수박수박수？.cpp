cf7b968eef1b11344d0d1d2389c058567cc6e19d
SooWon Kim <rla5233@naver.com>
2024-10-15 13:17:53 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.14 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+using namespace std;
+
+string solution(int n) 
+{
+    string answer = "";
+    for (int i = 0; i < n; ++i)
+    {
+        (i & 1) ? answer += "박" : answer += "수";
+    }
+
+    return answer;
+}
