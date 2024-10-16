335291b1bf8f2f2258b11781a2f38e8a5113448b
SooWon Kim <rla5233@naver.com>
2024-10-16 11:10:29 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 424.10 ms, Memory: 238 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
@@ -4,16 +4,16 @@
 std::vector<int> solution(std::vector<int> Numbers) 
 {
     std::vector<int> Result(Numbers.size(), -1);
-    std::stack<std::pair<int, int>> S;
+    std::stack<int> S;
     for (int i = 0; i < Numbers.size(); ++i)
     {
-        while (!S.empty() && S.top().first < Numbers[i])
+        while (!S.empty() && Numbers[S.top()] < Numbers[i])
         {
-            Result[S.top().second] = Numbers[i];
+            Result[S.top()] = Numbers[i];
             S.pop();
         }
 
-        S.push({ Numbers[i], i });
+        S.push(i);
     }
    
     return Result;
