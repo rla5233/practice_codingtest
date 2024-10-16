31353af270b87c64445c8152a2c85edacf5186e4
SooWon Kim <rla5233@naver.com>
2024-10-16 10:02:43 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.04 ms, Memory: 3.84 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> seoul) 
+{
+    int Index = std::distance(seoul.begin(), std::find(seoul.begin(), seoul.end(), "Kim"));
+    return "김서방은 " + to_string(Index) + "에 있다";
+}
