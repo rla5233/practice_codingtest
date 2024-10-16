99cbfc4126bf22792e30e09f3d8dbaf36feb5894
SooWon Kim <rla5233@naver.com>
2024-10-16 10:34:40 +0900

    [level 1] Title: 소수 찾기, Time: 3.94 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.cpp
@@ -1,11 +1,8 @@
 #include <vector>
 
-using namespace std;
-
 int solution(int n) 
 {
     int answer = 0;
-
     std::vector<bool> IsPrime(n + 1, true);
     for (int Num = 2; Num <= n; ++Num)
     {
@@ -13,13 +10,9 @@ int solution(int n)
         {
             for (int i = Num << 1; i < IsPrime.size(); i += Num)
                 IsPrime[i] = false;
-        }
-    }
-
-    for (int i = 2; i < IsPrime.size(); ++i)
-    {
-        if (IsPrime[i])
+            
             ++answer;
+        }
     }
 
     return answer;
