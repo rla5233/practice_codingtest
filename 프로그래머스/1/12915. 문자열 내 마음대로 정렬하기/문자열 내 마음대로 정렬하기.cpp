9411f84fe1e5a53a6296de81d0053d46b21561fd
SooWon Kim <rla5233@naver.com>
2024-10-17 16:41:16 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.04 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
@@ -6,12 +6,7 @@ std::vector<std::string> solution(std::vector<std::string> Strings, int N)
 {
     std::sort(Strings.begin(), Strings.end(), [&](const std::string& A, const std::string& B)
         {
-            if (A[N] < B[N])
-                return true;
-            else if (A[N] == B[N])
-                return A < B;
-            else
-                return false;
+            return (A[N] == B[N]) ? A < B : A[N] < B[N];
         }
     );
 
