8d9e451789b27dd59f4370bc49b614f68bc7431e
SooWon Kim <rla5233@naver.com>
2024-10-20 13:51:57 +0900

    [level 2] Title: 올바른 괄호, Time: 0.54 ms, Memory: 3.88 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <stack>
+
+bool solution(std::string S)
+{
+    std::stack<char> Stack;
+    for (char C : S)
+    {
+        if ('(' == C)
+            Stack.push(C);
+        else
+        {
+            if (Stack.empty() || '(' != Stack.top())
+                return false;
+            else
+                Stack.pop();
+        }
+    }
+
+    return Stack.empty() ? true : false;
+}
