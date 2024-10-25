7e7cffdb9646e7eb7f00e8ca4ccdca78c8039269
SooWon Kim <rla5233@naver.com>
2024-10-25 16:50:03 +0900

    [level 2] Title: 호텔 대실, Time: 0.82 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.cpp
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.cpp
@@ -1,52 +1,27 @@
 #include <algorithm>
 #include <string>
 #include <vector>
-#include <list>
+#include <queue>
 
-int solution(std::vector<std::vector<std::string>> Book_Time) 
+int solution(std::vector<std::vector<std::string>> Book_Time)
 {
-    std::vector<std::pair<int, int>> BookTime;
+    std::sort(Book_Time.begin(), Book_Time.end());
+    
+    int Result = 0;
+    std::priority_queue<int, std::vector<int>, std::greater<int>> Using;
     for (const auto& CurBook : Book_Time)
     {
-        std::string StartH = CurBook[0].substr(0, CurBook[0].find(':'));
-        std::string StartM = CurBook[0].substr(CurBook[0].find(':') + 1);
-        int Start = 60 * std::stoi(StartH) + std::stoi(StartM);
-        
-        std::string EndH = CurBook[1].substr(0, CurBook[1].find(':'));
-        std::string EndM = CurBook[1].substr(CurBook[1].find(':') + 1);
-        int End = 60 * std::stoi(EndH) + std::stoi(EndM);
-        
-        BookTime.push_back({ Start, End + 10 - Start });
+        std::string SStr = CurBook[0];
+        std::string EStr = CurBook[1];
+        int S = 60 * std::stoi(SStr.substr(0, 2)) + std::stoi(SStr.substr(3));
+        int E = 60 * std::stoi(EStr.substr(0, 2)) + std::stoi(EStr.substr(3)) + 10;
+    
+        while (!Using.empty() && Using.top() <= S)
+            Using.pop();
+
+        Using.push(E);
+        Result = std::max(Result, static_cast<int>(Using.size()));
     }
-    
-    std::sort(BookTime.begin(), BookTime.end());
-    
-    std::list<int> Using;
-    int Remain = 0, Result = 0, PrevTime = 0;
-    for (const auto& CurBook : BookTime)
-    {
-        int CurTime = CurBook.first;
-        int Diff = CurTime - PrevTime;
-        PrevTime = CurTime;
-        for (auto iter = Using.begin(); iter != Using.end();)
-        {
-            *iter -= Diff;
-            if(0 < *iter)
-                ++iter;
-            else
-            {
-                iter = Using.erase(iter);
-                ++Remain;
-            }
-        }
-        
-        if (0 < Remain)
-            --Remain;
-        else 
-            ++Result;
-        
-        Using.push_back(CurBook.second);        
-    }
-    
+
     return Result;
 }
