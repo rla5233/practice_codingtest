1f53f87a195243fe18411400ac30a2bffa87d4cb
SooWon Kim <rla5233@naver.com>
2024-10-29 16:44:28 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int solution(int N)
+{
+    int Result = 0;
+    while (0 != N)
+    {
+        if (N & 1)
+        {
+            --N;
+            ++Result;
+        }
+        else
+            N >>= 1;
+    }
+
+    return Result;
+}
