4ef450f10a173f3730a2b73856b26e172bdde62e
SooWon Kim <rla5233@naver.com>
2024-10-30 23:56:53 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -4,20 +4,9 @@ using namespace std;
 
 long long solution(int N) 
 {
-    if (1 == N)
-        return 1;
+    vector<long long> Jump(N + 1, 1);
+    for(int i = 0; i <= N - 2; ++i)
+        Jump[i + 2] = (Jump[i + 1] + Jump[i]) % 1234567;
     
-    if (2 == N)
-        return 2;
-    
-    long long Result = 0;
-    long long Temp1 = 2, Temp2 = 1;
-    for (int i = 3; i <= N; ++i)
-    {
-        Result = (Temp1 + Temp2) % 1234567;
-        Temp2 = Temp1;
-        Temp1 = Result;
-    }
-    
-    return Result;
+    return Jump[N];
 }
