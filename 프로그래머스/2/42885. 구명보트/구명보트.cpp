b5b22f795b2eb84f5e4493e6b4eed026b81ee577
SooWon Kim <rla5233@naver.com>
2024-10-30 16:33:30 +0900

    [level 2] Title: 구명보트, Time: 1.90 ms, Memory: 4.59 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> People, int Limit) 
+{
+    sort(People.begin(), People.end());
+
+    int Result = 0;
+    int Left = 0, Right = People.size() - 1;
+    while (Left <= Right)
+    {
+        if (Limit < People[Left] + People[Right])
+            --Right;
+        else
+        {
+            ++Left;
+            --Right;
+        }
+        
+        ++Result;
+    }
+
+    return Result;
+}
