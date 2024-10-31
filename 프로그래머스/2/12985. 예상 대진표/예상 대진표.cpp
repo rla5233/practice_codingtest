a85971c15ab9ee7f608d473f8b9a39769a50b5cf
SooWon Kim <rla5233@naver.com>
2024-10-31 12:55:16 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 4.28 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -1,24 +1,15 @@
-#include <algorithm>
-#include <vector>
+#include <iostream>
 
 using namespace std;
 
 int solution(int N, int A, int B)
 {
-    vector<int> AB = { A, B };
-    sort(AB.begin(), AB.end());
-    
-    int Result = 1;
-    while(AB[1] - AB[0] != 1 || !(AB[0] & 1))
+    A--, B--;
+    int Result = 0;
+    while(A != B)
     {
-        for (int& Num : AB)
-        {
-            if(Num & 1)
-                Num = (Num + 1) >> 1;
-            else
-                Num >>= 1;
-        }
-        
+        A >>= 1;
+        B >>= 1;
         ++Result;
     }
     
