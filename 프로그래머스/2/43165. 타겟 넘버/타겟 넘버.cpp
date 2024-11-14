d4791a4edc70a1e7c0f9a221da46e02a8474dd32
SooWon Kim <rla5233@naver.com>
2024-11-14 18:17:39 +0900

    [level 2] Title: 타겟 넘버, Time: 20.81 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,23 +1,58 @@
+#include <iostream>
 #include <string>
-#include <vector>
-
+#include <regex>
+#include <queue>
+#include <stack>
 using namespace std;
 
-void DFS(const vector<int>& _Numbers, const int _Target, int& _Answer, int _Depth, int _Sum)
-{
-    if (_Numbers.size() == _Depth)
-    {
-        _Answer += (_Target == _Sum);
-        return;
+int bfs(vector<int>& numbers, int target) {
+    int numsize = numbers.size();
+    std::queue<std::pair<int, int>> qu;
+    qu.push(make_pair(numbers[0], 0));
+    qu.push(make_pair(-numbers[0], 0));
+    std::pair<int, int> pqu;
+    int iqu = 0;
+    while (!qu.empty()) {
+        pqu = qu.front();
+        qu.pop();
+        if (pqu.second + 1 < numsize) {
+            qu.push(make_pair(pqu.first + numbers[pqu.second + 1], pqu.second + 1));
+            qu.push(make_pair(pqu.first - numbers[pqu.second + 1], pqu.second + 1));
+        }
+        else {
+            if (target == pqu.first) {
+                ++iqu;
+            }
+        }
     }
-    
-    DFS(_Numbers, _Target, _Answer, _Depth + 1, _Sum + _Numbers[_Depth]);
-    DFS(_Numbers, _Target, _Answer, _Depth + 1, _Sum - _Numbers[_Depth]);
+    return iqu;
 }
 
-int solution(vector<int> _Numbers, int _Target) 
-{
-    int Answer = 0;
-    DFS(_Numbers, _Target, Answer, 0, 0);
-    return Answer;
+int dfs(vector<int>& numbers, int target) {
+    int numsize = numbers.size();
+    std::stack<std::pair<int, int>> st;
+    st.push(make_pair(numbers[0], 0));
+    st.push(make_pair(-numbers[0], 0));
+    std::pair<int, int> pst;
+    int ist = 0;
+    while (!st.empty()) {
+        pst = st.top();
+        st.pop();
+        if (pst.second + 1 < numsize) {
+            st.push(make_pair(pst.first + numbers[pst.second + 1], pst.second + 1));
+            st.push(make_pair(pst.first - numbers[pst.second + 1], pst.second + 1));
+        }
+        else {
+            if (target == pst.first) {
+                ++ist;
+            }
+        }
+    }
+    return ist;
+}
+
+
+int solution(vector<int> numbers, int target) {
+
+    return bfs(numbers, target);
 }
