9541eb2ea2451c7ccabe8d33c15132a7285e84d0
SooWon Kim <rla5233@naver.com>
2024-11-15 15:52:36 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.28 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -12,13 +12,11 @@ int solution(vector<vector<int>> _Maps)
 {
     int H = _Maps.size(), W = _Maps[0].size();
     vector<vector<bool>> bVisit(H, vector<bool>(W, false));
-    vector<vector<int>> Cost(H, vector<int>(W, 0));
     int dy[4] = { -1, 1, 0 ,0 }, dx[4] = { 0, 0, -1, 1 };
     
     queue<pair<int, int>> Q;
     Q.push({ 0, 0 });
     bVisit[0][0] = true;
-    Cost[0][0] = 1;
     
     while (!Q.empty())
     {
@@ -30,14 +28,14 @@ int solution(vector<vector<int>> _Maps)
             int NextY = Now.first + dy[i], NextX = Now.second + dx[i];
             if (isRange(H, W, NextY, NextX))
             {
-                if (1 == _Maps[NextY][NextX] && !bVisit[NextY][NextX])
+                if (0 != _Maps[NextY][NextX] && !bVisit[NextY][NextX])
                 {
-                    Cost[NextY][NextX] = Cost[Now.first][Now.second] + 1;
+                    _Maps[NextY][NextX] = _Maps[Now.first][Now.second] + 1;
                     bVisit[NextY][NextX] = true; 
                     Q.push({ NextY, NextX });
                     
                     if (H - 1 == NextY && W - 1 == NextX)
-                        return Cost[NextY][NextX];
+                        return _Maps[NextY][NextX];
                 }
             }
         }
