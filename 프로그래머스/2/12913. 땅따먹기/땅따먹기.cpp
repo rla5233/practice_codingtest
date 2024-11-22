f0a805c59c1c7c08f9946a13139792d1f0a2e54d
SooWon Kim <rla5233@naver.com>
2024-11-22 20:28:01 +0900

    [level 2] Title: 땅따먹기, Time: 5.80 ms, Memory: 26 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
@@ -6,26 +6,13 @@ using namespace std;
 
 int solution(vector<vector<int>> land)
 {
-    vector<int> prevNum(4, 0);
-    for (int i = 0; i < 4; ++i)
-        prevNum[i] = land[0][i];
-
     for (int i = 1; i < static_cast<int>(land.size()); ++i)
     {
-        vector<int> curNum(4, 0);
-        for (int j = 0; j < static_cast<int>(land[i].size()); ++j)
-        {
-            curNum[j] = land[i][j] + max
-            ({ 
-                j != 0 ? prevNum[0] : 0,
-                j != 1 ? prevNum[1] : 0,
-                j != 2 ? prevNum[2] : 0,
-                j != 3 ? prevNum[3] : 0
-            });
-        }
-
-        prevNum = curNum;
+        land[i][0] += max(land[i - 1][1], max(land[i - 1][2], land[i - 1][3]));
+        land[i][1] += max(land[i - 1][0], max(land[i - 1][2], land[i - 1][3]));
+        land[i][2] += max(land[i - 1][0], max(land[i - 1][1], land[i - 1][3]));
+        land[i][3] += max(land[i - 1][0], max(land[i - 1][1], land[i - 1][2]));
     }
 
-    return *max_element(prevNum.begin(), prevNum.end());
+    return *max_element(land.back().begin(), land.back().end());
 }
