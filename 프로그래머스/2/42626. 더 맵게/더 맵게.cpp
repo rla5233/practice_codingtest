f974d91c33d135ba7a60f0bb7e1847a5a8c4276d
SooWon Kim <rla5233@naver.com>
2024-11-24 17:05:18 +0900

    [level 2] Title: 더 맵게, Time: 202.22 ms, Memory: 40.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -6,19 +6,19 @@ using namespace std;
 
 int solution(vector<int> scoville, int K) 
 {
-    priority_queue<int, vector<int>, greater<int>> pq;
-    for (int e : scoville)
-        pq.push(e);
+    priority_queue<int, vector<int>, greater<int>> pq(scoville.begin(), scoville.end());
     
     int answer = 0;
-    while (K > pq.top() && 1 < pq.size())
+    while (K > pq.top())
     {
+        if (1 == pq.size())
+            return -1;
+        
         int temp1 = pq.top(); pq.pop();
         int temp2 = pq.top(); pq.pop();
-        
         pq.push(temp1 + (2 * temp2));
         ++answer;
     }
        
-    return (1 != pq.size()) ? answer : (K > pq.top()) ? -1 : answer;
+    return answer;
 }
