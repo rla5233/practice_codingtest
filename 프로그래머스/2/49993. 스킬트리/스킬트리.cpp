54e5b0a73b0b1577414a89f3167c66df8f906a63
SooWon Kim <rla5233@naver.com>
2024-11-25 12:37:40 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string skill, vector<string> skill_trees) 
+{
+    vector<bool> bskill(26, false);
+    for (char c : skill)
+        bskill[c - 'A'] = true;
+    
+    int answer = static_cast<int>(skill_trees.size());
+    for (const auto& str : skill_trees)
+    {
+        int idx = 0;
+        for (char c : str)
+        {
+            if (bskill[c - 'A'])
+            {
+                if (skill[idx] != c)
+                {
+                    --answer;
+                    break;
+                }
+                
+                ++idx;
+            }
+        }
+    }
+    
+    return answer;
+}
