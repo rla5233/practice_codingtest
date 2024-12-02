8037b02ac58b01d5d3fe91ea4a505d87112f7402
SooWon Kim <rla5233@naver.com>
2024-12-02 17:19:31 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -2,22 +2,17 @@
 
 using namespace std;
 
-vector<long long> solution(vector<long long> numbers) {
+vector<long long> solution(vector<long long> numbers) 
+{
     vector<long long> answer;
-
-    for (long long x : numbers) 
+    for (long long number : numbers) 
     {
-        if (x % 2 == 0) 
-        {
-            answer.push_back(x + 1);
-        } 
-        else 
-        {
-            long long next = x + (x & -x);
-            long long adjust = ((x ^ next) / (x & -x)) >> 2;
-            answer.push_back(next | adjust);
-        }
+        long long bit = 1;
+        while ((number & bit) > 0) 
+            bit <<= 1;
+        
+        answer.push_back(number + bit - (bit >> 1));
     }
-
+    
     return answer;
 }
