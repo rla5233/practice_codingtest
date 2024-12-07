e84642c86c53d1a22a8e1468ea1b58ba3938eb23
HIGEON <rla5233@naver.com>
2024-12-07 20:08:33 +0900

    [level 2] Title: 소수 찾기, Time: 1.19 ms, Memory: 3.64 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -0,0 +1,60 @@
+#include <string>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+string temp = "";
+vector<bool> bUsed;
+unordered_set<int> dupCheck;
+
+bool isprime(int num)
+{
+    if (1 >= num)
+        return false;
+    
+    for (int i = 2; i * i <= num; ++i)
+    {
+        if (0 == num % i)
+            return false;
+    }
+    
+    return true;
+}
+
+void findfunc(const string& numbers, int cnt, int& ans)
+{
+    if (temp.size() == cnt)
+    {
+        int num = stoi(temp);
+        if (dupCheck.end() == dupCheck.find(num) && isprime(num))
+        {
+            dupCheck.insert(num);
+            ++ans;
+        }
+        
+        return;
+    }
+    
+    for (int i = 0; i < numbers.size(); ++i)
+    {
+        if (!bUsed[i])
+        {
+            temp.push_back(numbers[i]);
+            bUsed[i] = true;
+            findfunc(numbers, cnt, ans);
+            temp.pop_back();
+            bUsed[i] = false;
+        }
+    }
+}
+
+int solution(string numbers) 
+{
+    int answer = 0;
+    bUsed.resize(numbers.size(), false);
+    for (int i = 1; i <= numbers.size(); ++i)
+        findfunc(numbers, i, answer);
+    
+    return answer;
+}
